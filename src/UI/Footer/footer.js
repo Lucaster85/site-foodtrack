ace6a2619842cecc78221ef0e2bf183a51ddc7bd
lucaster <lucasdc87@gmail.com>
2021-11-12 00:27:58 -0300

    Footer first aproach

--- src/UI/Footer/footer.js
+++ src/UI/Footer/footer.js
@@ -2,10 +2,31 @@ import React from 'react';
 
 import * as styles from './footer.module.scss';
 
+import logo from '../../images/batman.png';
+
 const Footer = () => {
     return (
-        <footer>
-          © {new Date().getFullYear()}, Built with
+        <footer className={styles.footer}>
+          <div className={styles.main}>
+            <div className={styles.logo}>
+              <img src={logo} alt='Logo' />
+            </div>
+            <ol className={styles.lowerMenu}>
+              <li className={styles.menuItem}>Item nro 1</li>
+              <li className={styles.menuItem}>Item nro 2</li>
+              <li className={styles.menuItem}>Item nro 3</li>
+              <li className={styles.menuItem}>Item nro 4</li>
+            </ol>
+            <ol className={styles.social}>
+              <li className={styles.socialTitle}>Contact us on</li>
+              <li className={styles.socialItem}>Facebook</li>
+              <li className={styles.socialItem}>Wasap</li>
+              <li className={styles.socialItem}>Instagram</li>
+            </ol>
+          </div>
+          <div className={styles.copyright}>
+            © {new Date().getFullYear()}, Created by Lucaster.
+          </div>
         </footer>
     );
 }
